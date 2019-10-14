6b28016dee9bbe12c6a00dd525d3565f4b1e4921
prajaktay23 <prajaktayadav525@gmail.com>
2019-10-14 16:13:12 +0530

    varandlet

---
+++ videoprograms/valandlet.js
@@ -0,0 +1,23 @@
+let catName = "Quincy";
+let quote;
+
+ catName = "Beau";
+
+function catTalk(){
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
+}
+console.log(catTalk());
+ //let doesnt allow dublicate data
+
+
+//  //scopes of var and let keywords
+//  function checkscope(){
+//      "use strict";
+//      let i = "function scope";
+//      if(true){
+//          let
+//      }
+//  }2:40:48
