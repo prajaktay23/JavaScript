8ff9dbe61f14383530211fd278d85aab9f835159
prajaktay23 <prajaktayadav525@gmail.com>
2019-10-14 15:42:21 +0530

    parse function

---
+++ videoprograms/parseintfunction.js
@@ -0,0 +1,10 @@
+function convertToInteger(str){
+ return parseInt(str);
+}
+console.log(convertToInteger("56"));
+
+//binary no string into integer
+function convertintointegre(str){
+    return parseInt(str, 2 );
+}
+console.log(convertToInteger("10011"));
