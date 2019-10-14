3088cfcabe57ef33f379b45e8e8bf6d85a420691
prajaktay23 <prajaktayadav525@gmail.com>
2019-10-14 15:26:00 +0530

    random

---
+++ videoprograms/random.js
@@ -0,0 +1,14 @@
+//random fraction number
+function randomfraction(){
+    return Math.random();
+}
+console.log(randomfraction());
+ 
+//random whole number
+var randomnumberbetween0and19 =Math.floor(Math.random()* 20);
+
+function randomWholeNum(){
+
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum);
